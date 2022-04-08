11cce9b69b830c926fa8d38b4f1520bf997c5ac5
Jailson Paiva <j.psherl1009@gmail.com>
2022-04-08 19:38:59 -0400

    teste2

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ width: { max: 720},
 height: {max: 350 } } };
 
 navigator.mediaDevices.getUserMedia(constraints)
-.then(function(mediaStream) {
-  const video = document.querySelector('#video');
-  video.srcObject = mediaStream;
-  video.onloadedmetadata = function(e) {
+.then(function(stream) {
+    console.log(stream);
+    const video = document.querySelector('#video');
+    video.srcObject = stream;
     video.play();
-  };
+//     video.onloadedmetadata = function(e) {
+//   };
 })
 .catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
 
