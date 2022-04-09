82f7fcf8edef1362c905db3e44f37d64bee513af
Jailson Paiva <j.psherl1009@gmail.com>
2022-04-08 21:44:27 -0400

    tesetses

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import quagga  from './quagga.min.js';
+
 const flipButton = document.getElementById('flip-button')
 const video = document.querySelector('video');
 let front = false;
@@ -24,9 +26,29 @@ const camera = document.querySelector("#camera")
 
 camera.addEventListener("click", () => {
     const canvas = document.querySelector("#canvas")
-
-    canvas.height = "200px"
-    canvas.width = "200px"
     const context = canvas.getContext("2d")
+
+    canvas.height = video.videoHeight
+    canvas.width = video.videoWidth
     context.drawImage(video, 0, 0)
 })
+
+
+quagga.init({
+    inputStream : {
+        name : "Live",
+        type : "LiveStream",
+        target: document.querySelector('video')    // Or '#yourElement' (optional)
+    },
+    decoder : {
+        readers : ["code_128_reader"]
+    }
+    }, function(err) {
+        if (err) {
+            console.log(err);
+            return
+        }
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
+    }
+);
